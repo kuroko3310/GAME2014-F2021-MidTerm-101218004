2cda14edc413e1b9408984f0576f0ba7a033106c
Jm <kuroko3310@gmail.com>
2021-10-24 17:34:53 -0400

    Modified EnemyController Script ( positioned at the right side of the screen, rotated to face the player, made it move up and down, and added top and bottom boundaries.)

--- Assets/[Scripts]/EnemyController.cs
+++ Assets/[Scripts]/EnemyController.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections;
+﻿/*  Source file name: EnemyController.cs
+ *  Student Name: Jen Marc Capistrano
+ *  Student ID: 101218004
+ *  Date Last modified: October 24, 2021
+ *  Program Description: Everything's that happening on the enemy is in this script, such as movement
+ *  Revision History: v0.01 -- added variables for landscape orientations, and an offset values to add a unique position for each enemy
+ *                          -- made the enemy move up and down for landscape orientation, rotate the enemy to face the player, and added 
+ *                             top and bottom boundaries.
+ *                          
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +17,12 @@ public class EnemyController : MonoBehaviour
     public float horizontalSpeed;
     public float horizontalBoundary;
     public float direction;
-
+    // vertical variables
+    public float verticalSpeed;
+    public float verticalBoundary;
+    // position offset values
+    public float PosXOffset;
+    public float PosYOffset;
     // Update is called once per frame
     void Update()
     {
@@ -17,21 +32,50 @@ public class EnemyController : MonoBehaviour
 
     private void _Move()
     {
-        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+        if (Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            transform.position = new Vector3(PosXOffset, transform.position.y); // moves the enemy to the right side of the screen
+            transform.eulerAngles = new Vector3(0, 0, -90);     // rotates the enemy's direction to face the player
+            transform.position += new Vector3(0.0f,verticalSpeed * direction * Time.deltaTime,0.0f);   // make the enemy move up and down
+        }
+        else if (Screen.orientation == ScreenOrientation.Portrait)
+        {
+            transform.position = new Vector3(transform.position.x, PosYOffset);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+        }
+            
     }
 
     private void _CheckBounds()
     {
-        // check right boundary
-        if (transform.position.x >= horizontalBoundary)
+        if (Screen.orientation == ScreenOrientation.LandscapeRight)
         {
-            direction = -1.0f;
-        }
+            // check the top and bottom boundaries
+            if (transform.position.y >= verticalBoundary)
+            {
+                direction = -1.0f;
+            }
 
-        // check left boundary
-        if (transform.position.x <= -horizontalBoundary)
-        {
-            direction = 1.0f;
+            if (transform.position.y <= -verticalBoundary)
+            {
+                direction = 1.0f;
+            }
         }
+        else if (Screen.orientation == ScreenOrientation.Portrait)
+        {
+            // check right boundary
+            if (transform.position.x >= horizontalBoundary)
+            {
+                direction = -1.0f;
+            }
+
+            // check left boundary
+            if (transform.position.x <= -horizontalBoundary)
+            {
+                direction = 1.0f;
+            }
+        }
+        
     }
 }
